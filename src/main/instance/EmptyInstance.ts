28634b3d8f4dd5176af4943074eb9d546ea82fe6
Tiago Grosso <TiagoGrosso@users.noreply.github.com>
2023-12-18 16:13:54 +0000

    docs: add TSDoc to code (#19)
    Adds TSDoc documentation to code

--- src/main/instance/EmptyInstance.ts
+++ src/main/instance/EmptyInstance.ts
@@ -1,6 +1,12 @@
 import { Instance } from './Instance';
 
+/**
+ * An instance with no inner objects to run tasks that don't require an execution context.
+ */
 export class EmptyInstance implements Instance {
+    /**
+     * @inheritdoc
+     */
     shutdown(): void | Promise<void> {
         // Nothing to do on shutdown
     }
--- src/main/instance/Instance.ts
+++ src/main/instance/Instance.ts
@@ -1,3 +1,9 @@
+/**
+ * Represents an instance that can run tasks.
+ */
 export interface Instance {
+    /**
+     * Shuts down the instance.
+     */
     shutdown(): void | Promise<void>;
 }
